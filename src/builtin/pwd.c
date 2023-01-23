04d569838b75f938f2be1a24720aeb5d5033f5d7
jiwon0829 <hanjiwon8295@gmail.com>
2023-01-23 16:07:43 +0900

    feat: built-in func, pwd

---
+++ src/builtin/pwd.c
@@ -0,0 +1,14 @@
+#include "builtin.h"
+
+void	pwd(t_minishell *minishell, char **arr)
+{
+	char *path;
+
+	(void)arr;
+	path = getcwd(NULL, 0);
+	if (!path)
+		return ;
+	printf("%s\n", path);
+	free(path);
+	path = NULL;
+}

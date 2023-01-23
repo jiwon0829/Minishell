b9094058c5aac11abc83461d18be0411677d49c9
jiwon0829 <hanjiwon8295@gmail.com>
2023-01-23 16:15:58 +0900

    feat: built-in func, unset

---
+++ src/builtin/unset.c
@@ -0,0 +1,36 @@
+#include "builtin.h"
+
+static int check_variable_name(const char *name)
+{
+    if (!ft_isalpha(*name) && *name != '_')
+        return (FALSE);
+    while (*name)
+    {
+        if (!ft_isalpha(*name) && *name != '_' && !ft_isdigit(*name))
+            return (FALSE);
+        name++;
+    }
+    return (TRUE);
+}
+
+void	unset(t_minishell *minishell, char **arr)
+{
+    int i;
+
+    i = 1;
+    while (arr[i])
+    {
+        if (!check_variable_name(arr[i]))
+            unset_error_message(arr[i]);
+        else
+            delete_envp(&(minishell->envp), arr[i]);
+        ++i;
+    }
+    printf("unset result(%d)\n", minishell->envp->cnt);//TODO
+	t_envp *node = minishell->envp;
+	while (node)
+	{
+		printf("%s=%s\n", node->key, node->value);
+		node = node->next;
+	}
+}

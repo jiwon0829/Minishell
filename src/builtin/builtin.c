b143a4fe3e443d8bd8a9003128dbafdca2a74f6a
jiwon0829 <hanjiwon8295@gmail.com>
2023-02-02 18:12:34 +0900

    feat: built-in exit

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -19,17 +19,17 @@ t_cmd_tbl	*init_cmd_tbl(void)
 	if (!cmd_tbl->cmd)
 		exit(-1);	//TODO error message
 	setting_cmd(&cmd_tbl->cmd[0], ft_strdup("echo"), AC_ANY, "");
-	cmd_tbl->cmd[0].func = echo;
+	//cmd_tbl->cmd[0].func = echo;
 	setting_cmd(&cmd_tbl->cmd[1], "cd", AC_LESS_1, "");
 	//cmd_tbl->cmd[1].func = cd;
 	setting_cmd(&cmd_tbl->cmd[2], "pwd", AC_ZERO, "");
-	cmd_tbl->cmd[2].func = pwd;
+	//cmd_tbl->cmd[2].func = pwd;
 	setting_cmd(&cmd_tbl->cmd[3], "export", 0, "");
-	cmd_tbl->cmd[3].func = export;
+	//cmd_tbl->cmd[3].func = export;
 	setting_cmd(&cmd_tbl->cmd[4], "unset", 0, "");
-	cmd_tbl->cmd[4].func = unset;
+	//cmd_tbl->cmd[4].func = unset;
 	setting_cmd(&cmd_tbl->cmd[5], "env", 0, "");
-	cmd_tbl->cmd[5].func = env;
+	//cmd_tbl->cmd[5].func = env;
 	setting_cmd(&cmd_tbl->cmd[6], "exit", AC_ZERO, "");
 	cmd_tbl->cmd[6].func = ft_exit;
 	return (cmd_tbl);
@@ -48,20 +48,6 @@ int	check_builtin(t_cmd_tbl *cmd_tbl, const char *cmd)
 	return (FALSE);
 }
 
-int	check_builtin_arg(t_cmd *cmd, char **arr)
-{
-	int	argc = 0;
-	while (arr[argc])
-		argc++;
-	if ((cmd->argc == AC_ZERO && argc > 1) \
-		|| (cmd->argc == AC_LESS_1 && argc > 2))
-		{
-			printf("%s: too many arguments\n", arr[0]);
-			return (FALSE);
-		}
-	return (TRUE);
-}
-
 void	ft_execve(t_minishell *minishell, t_cmd_tbl *cmd_tbl, char **arr)
 {
 	int	i;
@@ -70,8 +56,8 @@ void	ft_execve(t_minishell *minishell, t_cmd_tbl *cmd_tbl, char **arr)
 	{
 		if (ft_strncmp(cmd_tbl->cmd[i].cmd, arr[0], ft_strlen(arr[0])) == 0)
 		{
-			if (check_builtin_arg(&cmd_tbl->cmd[i], arr))
-				cmd_tbl->cmd[i].func(minishell, arr);
+			cmd_tbl->cmd[i].func(minishell, arr);
+			return ;
 		}
 	}
 }

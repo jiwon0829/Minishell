0b87428b2782015cf230cd93d5be43b6da6c6406
Jiwon Han <jiwonhan@c5r9s5.42seoul.kr>
2023-02-13 09:49:10 +0900

    envp norm

--- src/envp/setting.c
+++ src/envp/setting.c
@@ -1,30 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setting.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiwonhan <jiwonhan@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 09:44:38 by jiwonhan          #+#    #+#             */
+/*   Updated: 2023/02/13 09:46:25 by jiwonhan         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "envp.h"
 #include "../libft/libft.h"
 
-void    get_key(t_envp *node, const char *envp)
+void	get_key(t_envp *node, const char *envp)
 {
-    int len;
+	int	len;
 
-    len = 0;
-    while (envp[len] && envp[len] != '=')
-        ++len;
-    node->key = ft_substr(envp, 0, len);
+	len = 0;
+	while (envp[len] && envp[len] != '=')
+		++len;
+	node->key = ft_substr(envp, 0, len);
 }
 
-void    get_value(t_envp *node, const char *envp)
+void	get_value(t_envp *node, const char *envp)
 {
-    int len;
+	int	len;
 
-    len = ft_strlen(envp) - ft_strlen(node->key) - 1;
-    node->value = ft_substr(envp, ft_strlen(node->key) + 1, len);
+	len = ft_strlen(envp) - ft_strlen(node->key) - 1;
+	node->value = ft_substr(envp, ft_strlen(node->key) + 1, len);
 }
 
-void    init_envp(t_envp **node, char **envp)
+void	init_envp(t_envp **node, char **envp)
 {
-    int		i;
-    t_envp  **head;
-    
-    head = node;
+	int		i;
+	t_envp	**head;
+
+	head = node;
 	i = 0;
 	while (envp[i])
 	{
@@ -36,33 +48,33 @@ void    init_envp(t_envp **node, char **envp)
 		++i;
 	}
 	*node = NULL;
-    (*head)->cnt = i;
+	(*head)->cnt = i;
 }
 
-char    **envp_to_dptr(t_envp *head)
+char	**envp_to_dptr(t_envp *head)
 {
-    char    **ret;
-    char    *str;
-    int     i;
+	char	**ret;
+	char	*str;
+	int		i;
 
-    ret = malloc(sizeof(char *) * (head->cnt + 1));
-    i = 0;
-    while (head)
-    {
-        ret[i] = ft_strjoin(head->key, "=");
-        str = ft_strjoin(ret[i], head->value);
-        free(ret[i]);
-        ret[i] = str;
-        head = head->next;
-        ++i;
-    }
-    ret[i] = NULL;
-    return (ret);
+	ret = malloc(sizeof(char *) * (head->cnt + 1));
+	i = 0;
+	while (head)
+	{
+		ret[i] = ft_strjoin(head->key, "=");
+		str = ft_strjoin(ret[i], head->value);
+		free(ret[i]);
+		ret[i] = str;
+		head = head->next;
+		++i;
+	}
+	ret[i] = NULL;
+	return (ret);
 }
 
-t_envp *get_envpNode(t_envp *head, const char *key)
+t_envp	*get_envpnode(t_envp *head, const char *key)
 {
-    while (head && ft_strncmp(head->key, key, ft_strlen(key) + 1))
-        head = head->next;
-    return (head);
+	while (head && ft_strncmp(head->key, key, ft_strlen(key) + 1))
+		head = head->next;
+	return (head);
 }

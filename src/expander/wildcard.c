0519ed7d20206063e7a71c741ac6e18b1e428acf
jiwon0829 <hanjiwon8295@gmail.com>
2023-02-13 10:41:32 +0900

    wildcard norm

--- src/expander/wildcard.c
+++ src/expander/wildcard.c
@@ -1,30 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiwonhan <jiwonhan@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 10:38:20 by jiwonhan          #+#    #+#             */
+/*   Updated: 2023/02/13 10:41:09 by jiwonhan         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "exec.h"
 #include "lexer.h"
 #include "parser.h"
 #include <dirent.h>
 #include "test_code.h"
 
-static int is_valid_value(char *dname, char *str)
+static int	is_valid_value(char *dname, char *str)
 {
-
-	int dname_len;
-	int str_len;
-	int i;
-	int d_i;
+	int	dname_len;
+	int	str_len;
+	int	i;
+	int	d_i;
 
 	dname_len = ft_strlen(dname);
 	str_len = ft_strlen(str);
 	i = 0;
 	while (i < dname_len && i < str_len && (dname[i] == str[i]))
 		++i;
-	if (str_len == i)	//str=k, dname=k -> return (TRUE)
-		return(dname_len == i);
+	if (str_len == i)
+		return (dname_len == i);
 	if (str[i] == '*')
 	{
 		d_i = 0;
 		while (i + d_i <= dname_len)
 		{
-			//if (is_valid_value(str + 1 + i, dname + d_i + i))
 			if (is_valid_value(dname + d_i + i, str + 1 + i))
 				return (1);
 			++d_i;
@@ -33,17 +43,18 @@ static int is_valid_value(char *dname, char *str)
 	return (0);
 }
 
-static int add_wildcard_token(t_token *token, char *dname, int *cnt)
+static int	add_wildcard_token(t_token *token, char *dname, int *cnt)
 {
-	char *str;
-	t_token *new;
+	char	*str;
+	t_token	*new;
 
 	if (!(*cnt))
 	{
 		str = ft_strdup(dname);
 		if (!str)
 			return (0);
-		free(token->value);token->value = NULL;
+		free(token->value);
+		token->value = NULL;
 		token->value = str;
 	}
 	else
@@ -58,57 +69,51 @@ static int add_wildcard_token(t_token *token, char *dname, int *cnt)
 	}
 	(*cnt)++;
 	return (1);
-
 }
 
-/*static void re_wildcard(char *str)
+static int	is_valid_wildcard(t_token *token, char *str)
 {
-	while (*str)
-	{
-		if (*str == '*')
-			*str *= -1;
-		++str;
-	}
-}*/
+	DIR				*dir;
+	struct dirent	*dirent;
+	int				cnt;
 
-static int is_valid_wildcard(t_token *token, char *str)
-{
-	DIR	*dir;
-	struct dirent *dirent;
-	int	cnt;
 	cnt = 0;
-	dir = opendir(".");	//지정한 디렉토리 열기(파일처럼 연다.)즉, 특정 디렉토리 안에 있는 파일과 디렉토리 검색을 위해 사용
+	dir = opendir(".");
 	if (!dir)
 		return (0);
-	while ((dirent = readdir(dir)))	//디렉토리의 처음부터 파일 또는 디렉토리명을 순서대로 한 개씩 읽는다
+	while (1)
 	{
-		if (is_valid_value(dirent->d_name, str) && !(add_wildcard_token(token, dirent->d_name, &cnt)))
+		dirent = readdir(dir);
+		if (!dirent)
+			break ;
+		if (is_valid_value(dirent->d_name, str) && \
+			!(add_wildcard_token(token, dirent->d_name, &cnt)))
 		{
 			closedir(dir);
 			return (0);
 		}
 	}
-	//if (!cnt)
-	//	re_wildcard(token->value);
 	if (closedir(dir) == -1)
 		return (0);
 	return (1);
 }
 
-static char *set_wildcard(char *value)
+static char	*set_wildcard(char *value)
 {
-	char *ret;
-	int ret_len;
-	int i=0;
-	char *tmp;
+	char	*ret;
+	int		ret_len;
+	int		i;
+	char	*tmp;
 
 	tmp = ft_strdup(value);
 	ret_len = 0;
+	i = 0;
 	while (value[i])
 	{
 		if (i == 0)
 			ret_len++;
-		else if (value[i] != '*' || (value[i] == '*' && tmp[ret_len - 1] != '*'))
+		else if (value[i] != '*' || \
+				(value[i] == '*' && tmp[ret_len - 1] != '*'))
 		{
 			tmp[ret_len] = value[i];
 			ret_len++;
@@ -122,9 +127,9 @@ static char *set_wildcard(char *value)
 	return (ret);
 }
 
-void    is_wildcard(t_token *token)
+void	is_wildcard(t_token *token)
 {
-	char *str;
+	char	*str;
 
 	if (!token)
 		return ;
